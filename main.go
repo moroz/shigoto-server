2074768d20f9a990237d8de4191584a05493c309
Karol Moroz <karol@moroz.dev>
2023-03-29 00:40:19 +0200

    Test fetching users

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"os"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/jmoiron/sqlx"
+	"github.com/moroz/shigoto-server/models"
 )
 
 func main() {
@@ -16,4 +20,13 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	ctx := context.Background()
+
+	users, err := models.Users().All(ctx, conn)
+	if err != nil {
+		panic(err)
+	}
+	json, _ := json.MarshalIndent(users, "", "  ")
+	fmt.Println(string(json))
 }
